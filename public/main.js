84e9aac320a2ae4a72e6602c45fd6d4903b7ce70
Alex Wildman <Alex>
2017-11-11 09:18:57 -0700

    working on sending completed tasks to db

--- public/main.js
+++ public/main.js
@@ -3,7 +3,6 @@ var mainVm = new Vue({
 	beforeCreate () {
 		$.get('/list', (currentlist) => {
 			console.log(currentlist)
-
 			this.listoftasks = currentlist
 		})
 	},
@@ -11,18 +10,44 @@ var mainVm = new Vue({
 		listoftasks: [],
     	newItem: {
 			userinput: '',
-		}
+			completed:false
+		},
     },
 	methods: {
-	postNewTask: function(event){
-		event.preventDefault()
-		// $().serialize() will grab all the named inputs in the form, and put their values into a url-encoded string
-		$.post('/newtask', {newItem: this.newItem}, (data) => {
-			// console.log(this.newitem)
-			this.listoftasks.push(data);
+		toggleCompleted: function(task) {
+			// var task = task
 
-		})
-	}
+			task.completed = !task.completed;
+			console.log(task)
+			// $.post('/update', task, function(updatedlist){
+			// 	this.listoftasks.push(updatedlist)
+			// 	console.log(updatedlist)
+				$.post('/update', {completed: this.completed}, (updatedlist) => {
+					this.newItem.completed = updatedlist
+				$.get('/list', (currentlist) => {
+					// console.log(currentlist)
+					this.listoftasks = currentlist
+				})
+			})
+		},
+
+		birthday: function(evt) {
+	            evt.preventDefault();
+	            $.post('/birthday', {birthdayAnimal: this.birthdayAnimal}, (dataFromServer) => {
+	                this.animals = dataFromServer;
+	            });
+	        },
+
+
+
+		postNewTask: function(event) {
+			event.preventDefault()
+			// $().serialize() will grab all the named inputs in the form, and put their values into a url-encoded string
+			$.post('/newtask', {newItem: this.newItem}, (data) => {
+				// console.log(this.newitem)
+				this.listoftasks.push(data);
+			})
+		},
 
 	},
 
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ mongoose.connect('mongodb://localhost:27017/mongo-todo')
 
 //create schema
 let newItem = new mongoose.Schema({
-		userinput: {type: String, required: true}
+		userinput: {type: String, required: true},
+		completed :{type:Boolean, default:false}
 	})
 //create model
 let InputModel = mongoose.model('todomodel', newItem)
@@ -32,13 +33,27 @@ app.get('/list', function(req, res){
 		}
 	)
 })
+app.post('/update', function(req, res) {
+	InputModel.update(
+		{ completed: req.body.completed},
+		function(err, update) {
+			console.log(update)
+			if (err) {
+				res.status(500).send(err);
+				return console.log(err)
+			}
+			res.send(update)
+		}
+	)
+})
 
 
 app.post('/newtask', function(req, res){
 
 	console.log(req.body);
 	let newItem = {
-		userinput: req.body.newItem.userinput
+		userinput: req.body.newItem.userinput,
+		completed: req.body.newItem.completed
 	}
 	new InputModel(newItem).save(function(err, addedTask){
 		if (err) {
